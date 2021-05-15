e90c32e4adc4e2557f88606f38ca255bb72abeaa
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-05-15 12:37:27 +0530

    added new shortcuts

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "touchbar-shortcuts" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('touchbar-shortcuts.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from touchbar-shortcuts!');
+	let fixAllImports = vscode.commands.registerCommand('touchbar-shortcuts.fix-all-imports', () => {
+		vscode.commands.executeCommand('dart-import.fix-all');
 	});
 
-	context.subscriptions.push(disposable);
+	let fixImports = vscode.commands.registerCommand('touchbar-shortcuts.fix-imports', () => {
+		vscode.commands.executeCommand('dart-import.fix');
+	});
+
+	let rename = vscode.commands.registerCommand('touchbar-shortcuts.rename', () => {
+		vscode.commands.executeCommand('editor.action.rename');
+	});
+
+	context.subscriptions.push(fixAllImports, fixImports, rename);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
