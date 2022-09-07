1fe45e0c19a15a68a9b09c51059270888231e22b
Sughosh Sudhanvan <bssughosh27@gmail.com>
2022-09-07 20:06:43 +0530

    formatted files

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,28 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  let fixAllImports = vscode.commands.registerCommand(
+    "touchbar-shortcuts.fix-all-imports",
+    () => {
+      vscode.commands.executeCommand("dart-import.fix-all");
+    }
+  );
 
-	let fixAllImports = vscode.commands.registerCommand('touchbar-shortcuts.fix-all-imports', () => {
-		vscode.commands.executeCommand('dart-import.fix-all');
-	});
+  let fixImports = vscode.commands.registerCommand(
+    "touchbar-shortcuts.fix-imports",
+    () => {
+      vscode.commands.executeCommand("dart-import.fix");
+    }
+  );
 
-	let fixImports = vscode.commands.registerCommand('touchbar-shortcuts.fix-imports', () => {
-		vscode.commands.executeCommand('dart-import.fix');
-	});
+  let rename = vscode.commands.registerCommand(
+    "touchbar-shortcuts.rename",
+    () => {
+      vscode.commands.executeCommand("editor.action.rename");
+    }
+  );
 
-	let rename = vscode.commands.registerCommand('touchbar-shortcuts.rename', () => {
-		vscode.commands.executeCommand('editor.action.rename');
-	});
-
-	context.subscriptions.push(fixAllImports, fixImports, rename);
+  context.subscriptions.push(fixAllImports, fixImports, rename);
 }
 
-export function deactivate() { }
+export function deactivate() {}
